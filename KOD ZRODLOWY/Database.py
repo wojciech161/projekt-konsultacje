3abf2015320dd5a93c773d22ee76c8e5d9cbb673
Marcin Wojciechowski <wojciech161@gmail.com>
2012-03-13 18:59:02 +0100

    Dodane kolejne funkcje do Database

--- KOD ZRODLOWY/Database.py
+++ KOD ZRODLOWY/Database.py
@@ -12,9 +12,9 @@ def connectToDatabase(host, user, password, databaseName):
 def closeDBConnection(dbHandle):
 	dbHandle.close()
 
-#Przykladowa funkcja wrzucajaca cos do bazy
-#(zebyscie wiedzieli jak pozniej do wyjebania)
-def insertToLocations(dbhandle, room, building):
+#Funkcja dodajaca lokalizacje do bazy danych
+#Zwraca 0 przy poprawnym zakonczeniu, -1 w blednym
+def addLocalization(dbhandle, room, building):
 	cursor = dbhandle.cursor()
 	
 	sqlquery = """INSERT INTO Localizations(room, building) values('%s', '%s')""" %(room, building)
@@ -25,35 +25,144 @@ def insertToLocations(dbhandle, room, building):
 	except:
 		print "Error with inserting data!"
 		dbhandle.rollback()
-
-#Przykladowa funkcja wyswietlajaca cos z bazy
-#(zebyscie wiedzieli jak pozniej do wyjebania)
-def printLocalizations(dbHandle):
-	cursor = dbHandle.cursor()
+		return -1
 	
-	sqlquery = """select * from Localizations"""
+	return 0
+
+#Funkcja zmieniajaca lokalizacje wg ID
+#Zwraca 0 przy poprawnym zakonczeniu, -1 w blednym
+def changeLocalization(dbhandle, localizationID, room, building):
+	cursor = dbhandle.cursor()
+	
+	sqlquery1 = """UPDATE Localizations set room = '%s' where localization_ID = %d""" %(room, localizationID)
+	sqlquery2 = """UPDATE Localizations set building = '%s' where localization_ID = %d""" %(building, localizationID)
+	
+	try:
+		cursor.execute(sqlquery1)
+		cursor.execute(sqlquery2)
+		dbhandle.commit()
+	except:
+		print "Error with changing localization"
+		dbhandle.rollback()
+		return -1
+	
+	return 0
+	
+#Funkcja zwracajaca dane o lokalizacji po podaniu ID
+def getLocalization(dbhandle, localizationID):
+	cursor = dbhandle.cursor()
+	
+	sqlquery = """select * from Localizations where localization_ID = %d""" %localizationID
 	
 	try:
 		cursor.execute(sqlquery)
-		
-		print "Localizations table"
-		print "id	room	building"
 		results = cursor.fetchall()
-		for row in results:
-			print "%s 	%s 	%s" %(row[0], row[1], row[2])
+		return results
 	except:
-		print "Error with reading data"
+		print "Error with getting localization"
+		return -1
+	
+
+#Funkcja dodajaca pusty InfoBoard
+#Zwraca 0 przy poprawnym zakonczeniu, -1 w blednym
+def addInfoBoard(dbhandle):
+	cursor = dbhandle.cursor()
+	
+	sqlquery = """INSERT INTO InfoBoards() values ()"""
+	
+	try:
+		cursor.execute(sqlquery)
+		dbhandle.commit()
+	except:
+		print "Error with adding InfoBoard"
+		dbhandle.rollback()
+		return -1
+	
+	return 0
+
+#Funkcja dodaje InfoBoard z wiadomoscia.
+#Zwraca 0 przy poprawnym zakonczeniu, -1 w blednym
+def addInfoBoardWithMessage(dbhandle, message):
+	cursor = dbhandle.cursor()
+	
+	sqlquery = """INSERT INTO InfoBoards(message) values ('%s')"""%message
+	
+	try:
+		cursor.execute(sqlquery)
+		dbhandle.commit()
+	except:
+		print "Error with adding InfoBoard"
+		dbhandle.rollback()
+		return -1
+	
+	return 0
+
+#Funkcja zmieniajaca wiadomosc w infoboard o danym id
+#Zwraca 0 przy poprawnym zakonczeniu, -1 w blednym
+def changeInfoBoard(dbhandle, infoBoardID, message):
+	cursor = dbhandle.cursor()
+	
+	sqlquery1 = """UPDATE InfoBoards set message = '%s' where infoboard_ID = %d""" %(message, infoBoardID)
+	sqlquery2 = """UPDATE InfoBoards set dateOfAdding = CURRENT_TIMESTAMP where infoboard_ID = %d""" %infoBoardID
+	
+	try:
+		cursor.execute(sqlquery1)
+		cursor.execute(sqlquery2)
+		dbhandle.commit()
+	except:
+		print "Error with changing InfoBoard"
+		dbhandle.rollback()
+		return -1
+	
+	return 0
+
+#Funkcja dodaje konsultacje
+#Zwraca 0 przy poprawnym zakonczeniu, -1 w blednym
+def addConsultation(dbhandle, tutorID, startHour, endHour, consultationDay, weekType, studentsLimit):
+	cursor = dbhandle.cursor()
+	
+	sqlquery = """INSERT INTO Consultations(
+	tutor_ID, startHour, endHour, consultation_day, weekType, studentsLimit) 
+	values ('%d', '%s', '%s', '%s', '%s', '%d');""" % (tutorID, startHour, endHour, consultationDay, weekType, studentsLimit)
+	
+	try:
+		cursor.execute(sqlquery)
+		dbhandle.commit()
+	except:
+		print "Error with adding consultation!"
+		dbhandle.rollback()
+		return -1
+	
+	return 0
+
+#Funkcja usuwa wszystkie konsultacje z bazy
+#Zwraca 0 przy poprawnym zakonczeniu, -1 w blednym
+def deleteAllConsultations(dbhandle):
+	cursor = dbhandle.cursor()
+	
+	sqlquery = """delete from Consultations"""
+	
+	try:
+		cursor.execute(sqlquery)
+		dbhandle.commit()
+	except:
+		print "Error with deleting all consultations"
+		dbhandle.rollback()
+		return -1
+	
+	return 0
 
 #Przyklad. Uzupelnijcie dane
+#Wszystko co ponizej bedzie do usuniecia w ostatecznym rozrachunku.
 host = "localhost"
-user = "UZYTKOWNIK"
-password = "HASLO"
-databaseName = "NAZWA BAZY"
+user = "pzuser"
+password = "pzpass"
+databaseName = "ProjektZespolowy"
 	
 dbcon = connectToDatabase(host, user, password, databaseName)
 
-#Odkomentowac co chcecie
-#insertToLocations(dbcon, "51", "C-3")
-#printLocalizations(dbcon)
+res = getLocalization(dbcon, 2)
+
+print res
 
 closeDBConnection(dbcon)
