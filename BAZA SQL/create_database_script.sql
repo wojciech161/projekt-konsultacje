23dfb86d59d59c5c4df09301ef1112e206c16cbe
Andrzej <lord_andre_metallica@yahoo.com>
2012-03-15 01:41:58 +0100

    Lekka modyfikacja skryptu tworzacego baze + skrypty kasujace tabele i insertujace poprawne dane testowe

--- BAZA SQL/create_database_script.sql
+++ BAZA SQL/create_database_script.sql
@@ -7,38 +7,64 @@ START TRANSACTION;
 CREATE TABLE IF NOT EXISTS Users (
 	user_ID int NOT NULL AUTO_INCREMENT,
 	login varchar(50),
-	lastLoginDate TIMESTAMP,
+	lastLoginDate timestamp DEFAULT 0,
 	type VARCHAR(15),
-	CONSTRAINT userIDpk PRIMARY KEY (user_ID)
+    
+	CONSTRAINT users_PK PRIMARY KEY (user_ID)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Students (
-	students_ID int NOT NULL AUTO_INCREMENT,
+	student_ID int NOT NULL,
 	name varchar(20),
 	surname varchar(50),
 	albumNumber int,
 	faculty varchar(40),
-	study_year int,
+	studyYear int,
 	major varchar(60),
-	CONSTRAINT studentIDpk PRIMARY KEY (students_ID)
+    
+	CONSTRAINT students_PK PRIMARY KEY (student_ID),
+  CONSTRAINT students_ID_FK FOREIGN KEY (student_ID) 
+                         REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Administrators (
-	administrator_ID int NOT NULL AUTO_INCREMENT,
+	administrator_ID int NOT NULL,
 	name varchar(20),
 	surname varchar(50),
-	CONSTRAINT administratorsIDpk PRIMARY KEY (administrator_ID)
+    
+	CONSTRAINT administrators_PK PRIMARY KEY (administrator_ID),
+  CONSTRAINT administrators_ID_FK FOREIGN KEY (administrator_ID)
+                          REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Assistants (
-	assistant_ID int NOT NULL AUTO_INCREMENT,
+	assistant_ID int NOT NULL,
 	name varchar(20),
 	surname varchar(50),
-	CONSTRAINT assistantIDpk PRIMARY KEY (assistant_ID)
+    
+	CONSTRAINT assistants_PK PRIMARY KEY (assistant_ID),
+  CONSTRAINT assistants_ID_FK FOREIGN KEY (assistant_ID)
+                          REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Infoboards (
+	infoboard_ID int NOT NULL AUTO_INCREMENT,
+	dateOfAdding timestamp DEFAULT 0,
+	message varchar(300),
+    
+	CONSTRAINT infoboards_PK PRIMARY KEY (infoboard_ID)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS Localizations (
+	localization_ID int NOT NULL AUTO_INCREMENT,
+	room varchar(8),
+	building varchar(8),
+    
+	CONSTRAINT localizationIDpk PRIMARY KEY (localization_ID)
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Tutors (
-	tutor_ID int NOT NULL AUTO_INCREMENT,
+	tutor_ID int NOT NULL,
 	degree varchar(40),
 	name varchar(20),
 	surname varchar(50),
@@ -46,100 +72,45 @@ CREATE TABLE IF NOT EXISTS Tutors (
 	phone varchar(20),
 	email varchar(50),
 	www varchar (60),
-	localization_ID int NOT NULL,
-	infoboard_ID int NOT NULL,
-	CONSTRAINT tutorIDpk PRIMARY KEY (tutor_ID)
-) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS ConsultationAssignments (
-	students_ID int NOT NULL,
-	consultations_ID int NOT NULL,
-	tutor_ID int NOT NULL
+	localization_ID int,
+	infoboard_ID int,
+    
+	CONSTRAINT tutors_PK PRIMARY KEY (tutor_ID),
+  CONSTRAINT tutors_ID_FK FOREIGN KEY (tutor_ID)
+                      REFERENCES Users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT tutors_localization_ID_FK FOREIGN KEY (localization_ID)
+                      REFERENCES Localizations(localization_id) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT tutors_infoboard_ID_FK FOREIGN KEY (infoboard_ID)
+                      REFERENCES Infoboards(infoboard_ID) ON DELETE SET NULL ON UPDATE SET NULL
 ) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS Consultations (
-	consultations_ID int NOT NULL AUTO_INCREMENT,
+	consultation_ID int NOT NULL AUTO_INCREMENT,
 	tutor_ID int NOT NULL,
 	startHour time,
 	endHour time,
-	consultation_day varchar(15),
+	day varchar(15),
 	weekType varchar(1),
 	studentsLimit int,
-	CONSTRAINT consultationIDpk PRIMARY KEY (consultations_ID)
+  localization_ID int,
+  
+	CONSTRAINT consultations_PK PRIMARY KEY (consultation_ID),
+  CONSTRAINT consultations_tutor_ID_FK FOREIGN KEY (tutor_ID)
+                     REFERENCES Tutors(tutor_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT consultations_localization_ID_FK FOREIGN KEY (localization_ID)
+                     REFERENCES Localizations(localization_ID) ON DELETE CASCADE ON UPDATE CASCADE               
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS Localizations (
-	localization_ID int NOT NULL AUTO_INCREMENT,
-	room varchar(8),
-	building varchar(8),
-	CONSTRAINT localizationIDpk PRIMARY KEY (localization_ID)
+CREATE TABLE IF NOT EXISTS ConsultationAssignments (
+	student_ID int NOT NULL,
+	consultation_ID int NOT NULL,
+  day date NOT NULL,
+
+  CONSTRAINT consultationAssignments_PK PRIMARY KEY (student_ID, consultation_ID),
+  CONSTRAINT consultationAssignments_student_ID_FK FOREIGN KEY (student_ID)   
+                      REFERENCES Students(student_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT consultationAssignments_consultation_ID_FK FOREIGN KEY (consultation_ID)   
+                      REFERENCES Consultations(consultation_id) ON DELETE CASCADE ON UPDATE CASCADE                   
 ) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS InfoBoards (
-	infoboard_ID int NOT NULL AUTO_INCREMENT,
-	dateOfAdding TIMESTAMP,
-	message varchar(300),
-	CONSTRAINT infoboardIDpk PRIMARY KEY (infoboard_ID)
-) ENGINE=InnoDB;
-
-ALTER TABLE Students ADD CONSTRAINT StudentsUsers_fk
-FOREIGN KEY (students_ID)
-REFERENCES Users (user_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE Administrators ADD CONSTRAINT AdministratorsUsers_fk
-FOREIGN KEY (administrator_ID)
-REFERENCES Users (user_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE Assistants ADD CONSTRAINT AssistantsUsers_fk
-FOREIGN KEY (assistant_ID)
-REFERENCES Users (user_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE Tutors ADD CONSTRAINT TutorsUsers_fk
-FOREIGN KEY (tutor_ID)
-REFERENCES Users (user_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE Consultations ADD CONSTRAINT ConsultationsTutors_fk
-FOREIGN KEY (tutor_ID)
-REFERENCES Tutors (tutor_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE ConsultationAssignments ADD CONSTRAINT CAConsultations_fk1
-FOREIGN KEY (consultations_ID)
-REFERENCES Consultations (consultations_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE ConsultationAssignments ADD CONSTRAINT CAConsultations_fk2
-FOREIGN KEY (tutor_ID)
-REFERENCES Consultations (tutor_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE ConsultationAssignments ADD CONSTRAINT CAStudents_fk
-FOREIGN KEY (students_ID)
-REFERENCES Students (students_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE Tutors ADD CONSTRAINT LocalizationsTutors_fk
-FOREIGN KEY (localization_ID)
-REFERENCES Localizations (localization_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
-
-ALTER TABLE Tutors ADD CONSTRAINT IBTuturs_fk
-FOREIGN KEY (infoboard_ID)
-REFERENCES InfoBoards (infoboard_ID)
-ON UPDATE CASCADE
-ON DELETE CASCADE;
 
 COMMIT;
---
+++ BAZA SQL/insert_test_data_script.sql
@@ -0,0 +1,48 @@
+INSERT INTO Users (user_ID, login, type)
+VALUES (1, 'jkowalski', 'student'), (2, 'jnowak', 'tutor'), (3, 'kabacki', 'student'),
+       (4, 'ametycki', 'student'), (5, 'topolski', 'student'), (6, 'mdebin', 'student'),
+       (7, 'dasist', 'assistant'), (8, 'madmin', 'administrator'), (9, 'lopek', 'tutor'),
+       (10, 'aabacki', 'tutor'), (11, 'aporywek', 'student'), (12, 'mopanek', 'tutor');
+   
+INSERT INTO Students (student_ID, name, surname, albumNumber, faculty, studyYear, major)
+VALUES (1, 'Jan', 'Kowalski', 112233, 'EKA', 3, 'INF'),
+       (3, 'Kazimierz', 'Abacki', 112244, 'EKA', 3, 'AIR'),
+       (4, 'Adam', 'Metycki', 112255, 'EKA', 2, 'INF'),
+       (5, 'Tomasz', 'Opolski', 112266, 'EKA', 1, 'AIR'),
+       (6, 'Mariusz', 'Debin', 112277, 'EKA', 3, 'EIT'),
+       (11, 'Amadeusz', 'Porywek', 112288, 'EKA', 4, 'TIN');
+   
+INSERT INTO Administrators (administrator_ID, name, surname)
+VALUES (8, 'Marek', 'Admin');
+
+INSERT INTO Assistants (assistant_ID, name, surname)
+VALUES (7, 'Daria', 'Asist');
+
+INSERT INTO Infoboards (infoboard_ID, message)
+VALUES (1, 'Pilna wiadomosc infoboard nr 1'), (2, 'Pilna wiadomosc infoboard nr 2'),
+       (3, 'Pilna wiadomosc infoboard nr 3'), (4, 'Pilna wiadomosc infoboard nr 4');
+    
+INSERT INTO Localizations (localization_ID, room, building)
+VALUES (1, '016', 'C3'), (2, '224', 'C3'), (3, '40', 'C4'), (4, '305', 'C3');
+
+INSERT INTO Tutors (tutor_ID, degree, name, surname, institute, phone, 
+                    email, www, localization_ID, infoboard_ID)
+VALUES (2, 'dr inz.', 'Jan', 'Nowak', 'ZAK', '071-321-22-22', 'Jan.Nowak@pwr.wroc.pl', NULL, 1, 1),
+(9, 'dr inz.', 'Leopold', 'Opek', 'ZSK', '071-321-21-21', 'Leopold.Opek@pwr.wroc.pl', NULL, 2, 2),
+(10, 'dr inz.', 'Adam', 'Abacki', 'IIAR', NULL, 'Adam.Abacki@pwr.wroc.pl', 'www.aabacki.pl', 3, 3),
+(12, 'prof.', 'Marian', 'Opanek', 'ZAK', '071-323-33-61', 'Marian.Opanek@pwr.wroc.pl', NULL, 4, 4);
+
+INSERT INTO Consultations (consultation_ID, tutor_ID, startHour, endHour, 
+                           day, weekType, studentsLimit, localization_ID)
+VALUES (1, 2, '09:15:00', '11:15:00', 'Poniedzialek', 'A', 15, 1),
+       (2, 2, '09:15:00', '11:15:00', 'Wtorek', 'A', 15, 1),
+       (3, 9, '11:15:00', '13:15:00', 'Czwartek', 'A', 15, 2),
+       (4, 9, '15:15:00', '17:15:00', 'Wtorek', 'A', 15, 2),
+       (5, 10, '13:15:00', '15:15:00', 'Sroda', 'A', 15, 3),
+       (6, 10, '17:15:00', '19:15:00', 'Piatek', 'A', 15, 3),
+       (7, 12, '09:15:00', '11:15:00', 'Poniedzialek', 'A', 15, 4),
+       (8, 12, '15:15:00', '17:15:00', 'Czwartek', 'A', 15, 4);
+       
+INSERT INTO ConsultationAssignments (student_ID, consultation_ID, day)
+VALUES (1, 1, '2012-03-12'), (3, 1, '2012-03-12'), (4, 2, '2012-03-13'), (5, 2, '2012-03-13');
+       
