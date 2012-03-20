be72aa27f925514227daf35c8009df94a109bfca
Maciek <szczerbacz@szczerbacz-F3Sg.(none)>
2012-03-20 20:13:26 +0100

    raz, raz, proba komita

--- KOD ZRODLOWY/table.py
+++ KOD ZRODLOWY/table.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 #encoding:UTF-8
 
+#Generowanie pliku z tabela HTML
+
 import sys
 import MySQLdb
-import csv
 
-con = MySQLdb.connect("localhost", "root", "", "projekt")
+connection = MySQLdb.connect("localhost", "root", "maciek1", "projekt")
 
 
-HTML=""
+HTML = "<table border='1'><tr valign='center'><td>Katedra</td><td>Prowadzący i terminy konsultacji</td></tr>\n"
 
-HTML= HTML + "<table border='1'><tr valign='center'><td>Katedra</td><td>Prowadzący i terminy konsultacji</td></tr>\n"
-
-c = con.cursor()
+c = connection.cursor()
 TR=""
+#Pobieranie instytutow
 c.execute("SELECT DISTINCT Institute FROM Tutors")
 for ins in c.fetchall ():
         TR = TR + "<tr>"
-	#1 kolumna - zaklady
+	#1 kolumna tabeli - instytuty(katedry)
 	TR = TR + "<td>%s</td>" % (ins[0])
 	query = "SELECT degree, name, surname, tutor_ID, phone, email, www  FROM Tutors WHERE Institute = '%s'" % (ins[0])
 	c.execute(query)
-	#2 kolumna - nazwiska
+	#2 kolumna tabeli - prowadzacy
 	TR = TR + "<td><table widht='100%' border='1' rules='rows' frame='void'>"
-	for kto in c.fetchall ():
-		TR=TR+"<tr><td>%s %s %s<br>%s<br>%s<br>%s</td><td><ul>" % (kto[0], kto[1], kto[2], kto[4], kto[6], kto[5])
-		#3 kolumna - godziny
-		query= "SELECT * FROM Consultations WHERE tutor_ID = '%s'" % (kto[3])
+	for prof in c.fetchall ():
+		TR=TR+"<tr><td>%s %s %s<br>%s<br>%s<br>%s</td><td><ul>" % (prof[0], prof[1], prof[2], prof[4], prof[6], prof[5])
+		#3 kolumna tabeli - terminy konsultacji
+		query= "SELECT * FROM Consultations WHERE tutor_ID = '%s'" % (prof[3]) #laczenie tabel - tutor_ID
 		c.execute(query)
 		for termin in c.fetchall():
 			TR= TR+ "<li>%s - %s, %s %s, %s</li>" % (termin[2], termin[3], termin[4], termin[5], termin[6])
@@ -36,7 +36,8 @@ for ins in c.fetchall ():
 
 HTML = HTML + TR
 
-print HTML
+#print HTML
+#Zapis tabeli do pliku table.html
 f = open('table.html', 'w')
 f.write(HTML)
 f.close()
