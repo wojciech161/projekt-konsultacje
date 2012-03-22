97e435a3b51bbbf1ee141a5546f51a0a05014408
Marcin Wojciechowski <wojciech161@gmail.com>
2012-03-22 18:55:04 +0100

    Dodalem widoki zgodne z templatem Romana

--- Django/konsultacje/consultations/views.py
+++ Django/konsultacje/consultations/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def consultation_index(request):
+	return HttpResponse("Lista wszystkich konsultacji")
+	
+def consultation_detail(request, consultation_id):
+	return HttpResponse("Konsultacja %s"%consultation_id)
+
+def tutors_index(request):
+	return HttpResponse("Lista wszystkich wykladowcow")
+
+def tutor_index(request, tutor_id):
+	return HttpResponse("Strona wykladowcy %s"%tutor_id)
+	
+def tutor_detail(request, tutor_id):
+	return HttpResponse("Edycja wykladowcy %s"%tutor_id)
+	
+def tutor_consultations(request, tutor_id):
+	return HttpResponse("Tu tutor %s bedzie mogl zobaczyc liste swoich konsultacji i ile osob jest zapisanych do niego(pozniej)"%tutor_id)
+	
+def edit_consultation(request, tutor_id, consultation_id):
+	return HttpResponse("Edycja konsultacji %s"%consultation_id)
+
+def authorization(request):
+	return HttpResponse("Ekran logowania")
--- Django/konsultacje/urls.py
+++ Django/konsultacje/urls.py
@@ -7,6 +7,14 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'konsultacje.views.home', name='home'),
     # url(r'^konsultacje/', include('konsultacje.foo.urls')),
+    url(r'^consultations/$', 'consultations.views.consultation_index'),
+    url(r'^consultations/(?P<consultation_id>\d+)/$', 'consultations.views.consultation_detail'),
+    url(r'^tutors/$', 'consultations.views.tutors_index'),
+    url(r'^tutor/(?P<tutor_id>\d+)/$', 'consultations.views.tutor_index'),
+    url(r'^tutor/(?P<tutor_id>\d+)/edit/$', 'consultations.views.tutor_detail'),
+    url(r'^tutor/(?P<tutor_id>\d+)/consultations/$', 'consultations.views.tutor_consultations'),
+    url(r'^tutor/(?P<tutor_id>\d+)/consultations/edit/(?P<consultation_id>\d+)/$', 'consultations.views.edit_consultation'),
+    url(r'^authorization/$', 'consultations.views.authorization'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
