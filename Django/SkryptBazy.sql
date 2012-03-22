6222833da2f93e99a0ef0064d568559a234b0b38
Marcin Wojciechowski <wojciech161@gmail.com>
2012-03-22 16:42:39 +0100

    Poprawiony blad z nullami

--- Django/SkryptBazy.sql
+++ Django/SkryptBazy.sql
@@ -1,87 +1,88 @@
 BEGIN;
-CREATE TABLE `consultations_users` (
+CREATE TABLE `consultations_user` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `login` varchar(50) NOT NULL,
-    `last_login_date` datetime NOT NULL
+    `last_login_date` datetime,
+    `typ` varchar(15) NOT NULL
 )
 ;
-CREATE TABLE `consultations_students` (
+CREATE TABLE `consultations_student` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `student_ID_id` integer NOT NULL,
     `name` varchar(20) NOT NULL,
     `surname` varchar(50) NOT NULL,
     `album_number` integer NOT NULL,
-    `faculty` varchar(40) NOT NULL,
-    `study_year` integer NOT NULL,
-    `major` varchar(60) NOT NULL
+    `faculty` varchar(40),
+    `study_year` integer,
+    `major` varchar(60)
 )
 ;
-ALTER TABLE `consultations_students` ADD CONSTRAINT `student_ID_id_refs_id_7730a2dc` FOREIGN KEY (`student_ID_id`) REFERENCES `consultations_users` (`id`);
-CREATE TABLE `consultations_administrators` (
+ALTER TABLE `consultations_student` ADD CONSTRAINT `student_ID_id_refs_id_eb0b6ca` FOREIGN KEY (`student_ID_id`) REFERENCES `consultations_user` (`id`);
+CREATE TABLE `consultations_administrator` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `administrator_ID_id` integer NOT NULL,
     `name` varchar(20) NOT NULL,
     `surname` varchar(50) NOT NULL
 )
 ;
-ALTER TABLE `consultations_administrators` ADD CONSTRAINT `administrator_ID_id_refs_id_13a35d7c` FOREIGN KEY (`administrator_ID_id`) REFERENCES `consultations_users` (`id`);
-CREATE TABLE `consultations_assistants` (
+ALTER TABLE `consultations_administrator` ADD CONSTRAINT `administrator_ID_id_refs_id_5bf5dfca` FOREIGN KEY (`administrator_ID_id`) REFERENCES `consultations_user` (`id`);
+CREATE TABLE `consultations_assistant` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `assistant_ID_id` integer NOT NULL,
     `name` varchar(20) NOT NULL,
     `surname` varchar(50) NOT NULL
 )
 ;
-ALTER TABLE `consultations_assistants` ADD CONSTRAINT `assistant_ID_id_refs_id_73696dd9` FOREIGN KEY (`assistant_ID_id`) REFERENCES `consultations_users` (`id`);
-CREATE TABLE `consultations_infoboards` (
+ALTER TABLE `consultations_assistant` ADD CONSTRAINT `assistant_ID_id_refs_id_7d319b79` FOREIGN KEY (`assistant_ID_id`) REFERENCES `consultations_user` (`id`);
+CREATE TABLE `consultations_infoboard` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `date_of_adding` datetime NOT NULL,
-    `message` varchar(300) NOT NULL
+    `message` varchar(300)
 )
 ;
-CREATE TABLE `consultations_localizations` (
+CREATE TABLE `consultations_localization` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `room` varchar(8) NOT NULL,
     `building` varchar(8) NOT NULL
 )
 ;
-CREATE TABLE `consultations_tutors` (
+CREATE TABLE `consultations_tutor` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `tutor_ID_id` integer NOT NULL,
-    `degree` varchar(40) NOT NULL,
+    `degree` varchar(40),
     `name` varchar(20) NOT NULL,
     `surname` varchar(50) NOT NULL,
     `institute` varchar(50) NOT NULL,
-    `phone` varchar(20) NOT NULL,
+    `phone` varchar(20),
     `email` varchar(50) NOT NULL,
-    `www` varchar(60) NOT NULL,
+    `www` varchar(60),
     `localization_ID_id` integer NOT NULL,
     `infoboard_ID_id` integer NOT NULL
 )
 ;
-ALTER TABLE `consultations_tutors` ADD CONSTRAINT `tutor_ID_id_refs_id_2fbbb1bb` FOREIGN KEY (`tutor_ID_id`) REFERENCES `consultations_users` (`id`);
-ALTER TABLE `consultations_tutors` ADD CONSTRAINT `localization_ID_id_refs_id_4b238d2b` FOREIGN KEY (`localization_ID_id`) REFERENCES `consultations_localizations` (`id`);
-ALTER TABLE `consultations_tutors` ADD CONSTRAINT `infoboard_ID_id_refs_id_b1bfd77f` FOREIGN KEY (`infoboard_ID_id`) REFERENCES `consultations_infoboards` (`id`);
-CREATE TABLE `consultations_consultations` (
+ALTER TABLE `consultations_tutor` ADD CONSTRAINT `localization_ID_id_refs_id_c848e499` FOREIGN KEY (`localization_ID_id`) REFERENCES `consultations_localization` (`id`);
+ALTER TABLE `consultations_tutor` ADD CONSTRAINT `tutor_ID_id_refs_id_a8937665` FOREIGN KEY (`tutor_ID_id`) REFERENCES `consultations_user` (`id`);
+ALTER TABLE `consultations_tutor` ADD CONSTRAINT `infoboard_ID_id_refs_id_77c84a0d` FOREIGN KEY (`infoboard_ID_id`) REFERENCES `consultations_infoboard` (`id`);
+CREATE TABLE `consultations_consultation` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `tutor_ID_id` integer NOT NULL,
     `start_hour` datetime NOT NULL,
     `end_hour` datetime NOT NULL,
     `day` varchar(15) NOT NULL,
     `week_type` varchar(1) NOT NULL,
-    `students_limit` integer NOT NULL,
+    `students_limit` integer,
     `localization_ID_id` integer NOT NULL
 )
 ;
-ALTER TABLE `consultations_consultations` ADD CONSTRAINT `localization_ID_id_refs_id_ccbdf76f` FOREIGN KEY (`localization_ID_id`) REFERENCES `consultations_localizations` (`id`);
-ALTER TABLE `consultations_consultations` ADD CONSTRAINT `tutor_ID_id_refs_id_53b68a33` FOREIGN KEY (`tutor_ID_id`) REFERENCES `consultations_tutors` (`id`);
-CREATE TABLE `consultations_consultationassignments` (
+ALTER TABLE `consultations_consultation` ADD CONSTRAINT `localization_ID_id_refs_id_ab94da9b` FOREIGN KEY (`localization_ID_id`) REFERENCES `consultations_localization` (`id`);
+ALTER TABLE `consultations_consultation` ADD CONSTRAINT `tutor_ID_id_refs_id_16147b69` FOREIGN KEY (`tutor_ID_id`) REFERENCES `consultations_tutor` (`id`);
+CREATE TABLE `consultations_consultationassignment` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `student_ID_id` integer NOT NULL,
     `consultation_ID_id` integer NOT NULL,
     `day` datetime NOT NULL
 )
 ;
-ALTER TABLE `consultations_consultationassignments` ADD CONSTRAINT `consultation_ID_id_refs_id_6c13cb60` FOREIGN KEY (`consultation_ID_id`) REFERENCES `consultations_consultations` (`id`);
-ALTER TABLE `consultations_consultationassignments` ADD CONSTRAINT `student_ID_id_refs_id_b093e067` FOREIGN KEY (`student_ID_id`) REFERENCES `consultations_students` (`id`);
+ALTER TABLE `consultations_consultationassignment` ADD CONSTRAINT `student_ID_id_refs_id_e2ac55c3` FOREIGN KEY (`student_ID_id`) REFERENCES `consultations_student` (`id`);
+ALTER TABLE `consultations_consultationassignment` ADD CONSTRAINT `consultation_ID_id_refs_id_9b95c80e` FOREIGN KEY (`consultation_ID_id`) REFERENCES `consultations_consultation` (`id`);
 COMMIT;
--- Django/konsultacje/consultations/models.py
+++ Django/konsultacje/consultations/models.py
@@ -2,58 +2,58 @@ from django.db import models
 
 class User(models.Model):
 	login = models.CharField(max_length = 50)
-	last_login_date = models.DateTimeField()
+	last_login_date = models.DateTimeField('Data ostatniego logowania', null=True)
 	typ = models.CharField(max_length = 15)
 	
 class Student(models.Model):
 	student_ID = models.ForeignKey(User)
-	name = models.CharField(max_length = 20)
-	surname = models.CharField(max_length = 50)
-	album_number = models.IntegerField()
-	faculty = models.CharField(max_length = 40)
-	study_year = models.IntegerField()
-	major = models.CharField(max_length = 60)
+	name = models.CharField('Imie', max_length = 20)
+	surname = models.CharField('Nazwisko', max_length = 50)
+	album_number = models.IntegerField('Nr indeksu')
+	faculty = models.CharField('Wydzial', max_length = 40, null=True)
+	study_year = models.IntegerField('Rok studiow', null=True)
+	major = models.CharField('Kierunek', max_length = 60, null=True)
 
 class Administrator(models.Model):
 	administrator_ID = models.ForeignKey(User)
-	name = models.CharField(max_length = 20)
-	surname = models.CharField(max_length = 50)
+	name = models.CharField('Imie', max_length = 20)
+	surname = models.CharField('Nazwisko', max_length = 50)
 
 class Assistant(models.Model):
 	assistant_ID = models.ForeignKey(User)
-	name = models.CharField(max_length = 20)
-	surname = models.CharField(max_length = 50)
+	name = models.CharField('Imie', max_length = 20)
+	surname = models.CharField('Nazwisko', max_length = 50)
 
 class InfoBoard(models.Model):
-	date_of_adding = models.DateTimeField()
-	message = models.CharField(max_length = 300)
+	date_of_adding = models.DateTimeField('Data dodania')
+	message = models.CharField('Tresc wiadomosci', max_length = 300, null=True)
 
 class Localization(models.Model):
-	room = models.CharField(max_length = 8)
-	building = models.CharField(max_length = 8)
+	room = models.CharField('Pokoj', max_length = 8)
+	building = models.CharField('Budynek', max_length = 8)
 
 class Tutor(models.Model):
 	tutor_ID = models.ForeignKey(User)
-	degree = models.CharField(max_length = 40)
-	name = models.CharField(max_length = 20)
-	surname = models.CharField(max_length = 50)
-	institute = models.CharField(max_length = 50)
-	phone = models.CharField(max_length = 20)
-	email = models.CharField(max_length = 50)
-	www = models.CharField(max_length = 60)
+	degree = models.CharField('Stopien naukowy', max_length = 40, null=True)
+	name = models.CharField('Imie', max_length = 20)
+	surname = models.CharField('Nazwisko', max_length = 50)
+	institute = models.CharField('Jednostka organizacyjna', max_length = 50)
+	phone = models.CharField('Telefon', max_length = 20, null=True)
+	email = models.CharField('E-mail', max_length = 50)
+	www = models.CharField('WWW', max_length = 60, null=True)
 	localization_ID = models.ForeignKey(Localization)
 	infoboard_ID = models.ForeignKey(InfoBoard)
 
 class Consultation(models.Model):
 	tutor_ID = models.ForeignKey(Tutor)
-	start_hour = models.DateTimeField()
-	end_hour = models.DateTimeField()
-	day = models.CharField(max_length = 15)
-	week_type = models.CharField(max_length = 1)
-	students_limit = models.IntegerField()
+	start_hour = models.DateTimeField('Godzina rozpoczecia')
+	end_hour = models.DateTimeField('Godzina zakonczenia')
+	day = models.CharField('Dzien', max_length = 15)
+	week_type = models.CharField('Typ tygodnia', max_length = 1)
+	students_limit = models.IntegerField('Limit', null=True)
 	localization_ID = models.ForeignKey(Localization)
 
 class ConsultationAssignment(models.Model):
 	student_ID = models.ForeignKey(Student)
 	consultation_ID = models.ForeignKey(Consultation)
-	day = models.DateTimeField()
+	day = models.DateTimeField('Dzien')
