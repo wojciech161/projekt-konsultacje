7184ee851b18a69f146756d329bf54129322e4a8
Marcin Wojciechowski <wojciech161@gmail.com>
2012-03-24 10:40:52 +0100

    commit speszali for lukasz

--- Django/konsultacje/settings.py
+++ Django/konsultacje/settings.py
@@ -1,4 +1,7 @@
 # Django settings for konsultacje project.
+
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,8 +15,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ProjektZespolowy',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'shogun13',                  # Not used with sqlite3.
+        'USER': 'pzuser',                      # Not used with sqlite3.
+        'PASSWORD': 'pzpass',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -105,7 +108,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "E:\Lukasz\polibuda\projekt_zespolowy\Django2\konsultacje\Templates"
+    os.path.join(os.path.dirname(__file__), 'Templates')
 )
 
 INSTALLED_APPS = (
